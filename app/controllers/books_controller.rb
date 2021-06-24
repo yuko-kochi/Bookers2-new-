c8f69d5d9639c39d7c212594693ecc446f353fa9
happy.k165131@gmail.com <ec2-user@ip-172-31-57-111.ec2.internal>
2021-06-24 06:17:12 +0000

    [Add]Books

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,2 +1,31 @@
 class BooksController < ApplicationController
+
+  def index
+    @book = Book.new
+    @books = Book.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to books_path
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+
 end
