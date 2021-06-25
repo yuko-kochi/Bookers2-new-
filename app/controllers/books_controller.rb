cdc14c6c0d88a2a375354b6f5f744c53d1c39cc1
happy.k165131@gmail.com <ec2-user@ip-172-31-57-111.ec2.internal>
2021-06-25 11:27:35 +0000

    [Add]image

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,7 @@ class BooksController < ApplicationController
   def show
     @book = Book.new
     @book_id = Book.find(params[:id])
-    @user = User.find(params[:id])
+    @user = @book_id.user
   end
 
   def create
